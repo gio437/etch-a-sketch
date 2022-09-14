c2d91d44059711824babddf7a1470ee1730ae134
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-09-14 19:48:10 -0400

    updates

--- javascript.js
+++ javascript.js
@@ -7,27 +7,41 @@
 let color = 0; 
 
 function createSquare() {
+
+    for (let i = 0; i < 16; i++) {
     let box = document.querySelector(".grid");
 
     let square = document.createElement("div");
     square.classList.add("square");
 
     box.appendChild(square); 
+    }
     
 }
 
-createSquare();
-   //makes the squares
-    //makes hover function
+    createSquare(); //makes the squares
+   
 
 
+
+    function playGame() {
+    
     let square = document.querySelector(".square");
+
+
     square.addEventListener("mouseover", changeColor);
+    
+}
+
+    playGame();
 
 
 function changeColor() {
-    let hello = document.querySelector(".square");
-    hello.style.color = "blue";
+    
+    let hello = document.querySelectorAll(".square");
 
+    for (let i = 0; i < hello.length; i++){
+    hello[i].style.color = "blue";
+    }
 }
 
