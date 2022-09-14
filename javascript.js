7fd3f66486fa1f12a8ae329953dac3e0915d1e46
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-09-14 17:05:15 -0400

    basic formula

--- javascript.js
+++ javascript.js
@@ -2,17 +2,32 @@
 // use flexbox to put boxes together
 // create eventListener that will change css.style.color -function
 // eventListener "hover"^
-//create button that will change color selection -function
+// create button that will change color selection -function
 
 let color = 0; 
 
 function createSquare() {
-    let box = document.querySelector(".grid")
+    let box = document.querySelector(".grid");
 
     let square = document.createElement("div");
     square.classList.add("square");
 
     box.appendChild(square); 
+    
 }
 
 createSquare();
+   //makes the squares
+    //makes hover function
+
+
+    let square = document.querySelector(".square");
+    square.addEventListener("mouseover", changeColor);
+
+
+function changeColor() {
+    let hello = document.querySelector(".square");
+    hello.style.color = "blue";
+
+}
+
