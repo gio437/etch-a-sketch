8606127cf7010a407b427e6e75fce46cef6742b8
Giovanni S <giomail33@gmail.com>
2022-09-13 20:02:05 -0400

    psedocode

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
+// put each row in a div
+// use flexbox to put boxes together
+// create eventListener that will change css.style.color -function
+// eventListener "hover"^
+//create button that will change color selection -function
+
 let color = 0; 
 
 function createSquare() {
-    let box = document.querySelector("grid")
+    let box = document.querySelector(".grid")
 
     let square = document.createElement("div");
-    square.setAttribute("square");
-    
+    square.classList.add("square");
+
     box.appendChild(square); 
 }
 
