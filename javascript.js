7af52ec2726c7f403fa48f7f4d9c90edcec8ba21
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-09-15 14:11:10 -0400

    functional script

--- javascript.js
+++ javascript.js
@@ -36,4 +36,29 @@ function createSquare() {
 
     playGame(); //colors squares on mouseover
 
+// create arrow function to remove querySelector all .square -function
+// use loop right under to add .square back to the DOM -function
 
+let press = document.querySelector(".reset");
+press.addEventListener("click", changeSquares);
+
+function changeSquares() {
+    let amount = window.prompt();
+
+    let box = document.querySelectorAll(".square");
+    
+    if (amount <= 100){
+    for (let i = 0; i < 256; i++) {
+        box[i].remove();
+}
+    
+    for (let i = 0; i < amount * amount; i++) {
+        let box = document.querySelector(".grid");
+
+        let square = document.createElement("div");
+        square.classList.add("square");
+
+        box.appendChild(square); 
+    }
+    }
+}
