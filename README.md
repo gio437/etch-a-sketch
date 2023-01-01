562c4f5f9f04e03ac479402fb57c213f2e54b5b2
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-01-01 18:22:01 -0500

    If applied will fix script code spacing and add read me

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+Classic etch-a-sketch, click a color than start drawing. click reset to click the board as well as pick a number 1-100 which will change the size of the drawing board
--- javascript.js
+++ javascript.js
@@ -7,24 +7,16 @@
 // use variable for extra credit -var
 
 color = 0; //maybe add functionality to allow it  to remember the color after reset?
-
 function createSquare() {
-
     for (let i = 0; i < 256; i++) {
     let box = document.querySelector(".grid");
-
     let square = document.createElement("div");
     square.classList.add("square");
-
-    box.appendChild(square); 
+    box.appendChild(square);
     }
-    
+
 }
-
     createSquare(); //makes the squares
-   
-
-    
 
     function playGame() {
     color = 1;
@@ -43,7 +35,6 @@ press3.addEventListener("click", playGame);  //colors squares on mouseover
 // create arrow function to remove querySelector all .square -function
 // use loop right under to add .square back to the DOM -function
 
-
 let press = document.querySelector(".reset");
 press.addEventListener("click", changeSquares);
 
@@ -53,25 +44,25 @@ function changeSquares() {
     let user = 300 / amount;
 
     let box = document.querySelectorAll(".square");
-    
-    
+
+
     if (amount <= 100 && amount > 0){
     for (let i = 0; i < box.length; i++) {
         box[i].remove();
 }
-    
+
     for (let i = 0; i < amount * amount; i++) {
         let grid = document.querySelector(".grid");
 
         let square = document.createElement("div");
         square.classList.add("square");
-       
+
         box.forEach((e) => {
             square.style.height = user.toString() + "px";
             square.style.width = user.toString() + "px";
         });
-        
-        grid.appendChild(square); 
+
+        grid.appendChild(square);
 
     }
     if (color == 1) {
@@ -97,33 +88,30 @@ function createRainbow() {
     colorful.forEach(colorful => {
         colorful.addEventListener("mouseover", (e)=>{
         colorful.style.background = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
-        
+
     });
    });
-  
+
 }
 
-function createBlack() {
-    //let opacity = 0.1;
+// function createBlack() {
+//     //let opacity = 0.1;
 
-   
-    let black = document.querySelectorAll(".square");
-    black.forEach(black => {
-        black.addEventListener("mouseover", (e)=>{
-        //black.style.background = `rgba(000, 000, 000, ${opacity})`;
-        //let shade = window.getComputedStyle(black);
 
-        //let currentOpacity = shade.getPropertyValue(`opacity`) + 0.1;
-        //console.log(currentOpacity);
-        //black.style.background = `rgba(000, 000, 000, ${currentOpacity})`
-    });
-    });
+//     let black = document.querySelectorAll(".square");
+//     black.forEach(black => {
+//         black.addEventListener("mouseover", (e)=>{
+//         //black.style.background = `rgba(000, 000, 000, ${opacity})`;
+//         //let shade = window.getComputedStyle(black);
 
-    
+//         //let currentOpacity = shade.getPropertyValue(`opacity`) + 0.1;
+//         //console.log(currentOpacity);
+//         //black.style.background = `rgba(000, 000, 000, ${currentOpacity})`
+//     });
+//     });
 
-   
-}
+// }
 
 //createBlack();
 
-//if 0.1 = 0.2 window.getComputedStyle(black) passes element to it, returns style object, 
+//if 0.1 = 0.2 window.getComputedStyle(black) passes element to it, returns style object,
